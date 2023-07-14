16b95c365bc7ef4c558b9c992f1f9fa65a26c329
Derrak Richard <derrak@gmail.com>
2023-07-14 10:39:51 -0700

    add state to form inputs

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -21,6 +21,17 @@ export default function Meme() {
     }))
   }
 
+  function handleChange(event) {
+    //console.log(event)
+    const { name, value, type } = event.target
+    setMeme(prevMemeData => {
+      return {
+        ...prevMemeData,
+        [name]: value
+      }
+    })
+  }
+
   return (
     <main className="form--main">
       <div className="form">
@@ -28,11 +39,17 @@ export default function Meme() {
           type="text"
           placeholder="Top text"
           className="form--input"
+          onChange={handleChange}
+          name="topText"
+          value={meme.topText}
         />
         <input
           type="text"
           placeholder="Bottom text"
           className="form--input"
+          onChange={handleChange}
+          name="bottomText"
+          value={meme.bottomText}
         />
         <button
           className="form--button"
@@ -43,8 +60,8 @@ export default function Meme() {
       </div> 
       <div className="meme">
         <img src={meme.randomImage} className="meme--image" alt="" />
-        <h2 className="meme--text top">One does not simply</h2>
-        <h2 className="meme--text bottom">Walk into Mordor</h2>
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   )
