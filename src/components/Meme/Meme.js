11e1d39c6744b99af8b539e79acae9355a9aadf0
Derrak Richard <derrak@gmail.com>
2023-07-14 22:21:19 -0700

    update useEffect to asyncAwait

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -12,10 +12,13 @@ export default function Meme() {
   const [allMemes, setAllMemes] = React.useState(memesData)
 
   React.useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then(res => res.json())
-      .then(data => setAllMemes(data.data.memes))
-  },[])
+    async function getMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes")
+      const data = await res.json()
+      setAllMemes(data.data.memes)
+    }
+    getMemes()
+  }, [])
 
   function getMemeImage() {
     const randomNumber = Math.floor(Math.random() * allMemes.length)
